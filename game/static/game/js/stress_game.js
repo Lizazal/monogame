417a218810b18331759f96d2af026cf1b5e2a56e
Lizazal <66370193+Lizazal@users.noreply.github.com>
2023-01-22 22:12:52 +0300

    Add files via upload

--- game/static/game/js/stress_game.js
+++ game/static/game/js/stress_game.js
@@ -382,7 +382,7 @@ function update(context, monorings, gameState) {
         context.fillText("Нажмите 's' для начала игры", CENTER_X, CENTER_Y/2);
         context.fillText("В момент пересечения черты кружком нажимайте на соответствующую клавишу", CENTER_X, 4*CENTER_Y/6);
         context.fillText(`Нажимайте '${monorings.left.key}' для кружка слева, '${monorings.middle.key}' для кружка по центру и '${monorings.right.key}' для кружка справа`, CENTER_X, 5*CENTER_Y/6);
-        context.fillText(`Ига остановится при достижении 85% ошибок хотя бы у одного кружка`, CENTER_X, 6*CENTER_Y/6);
+        context.fillText(`Игра остановится при достижении 85% ошибок хотя бы у одного кружка`, CENTER_X, 6*CENTER_Y/6);
     } else {
         if (gameState.gameEnded) {
             gameState.allEnded = gameState.allEnded || monorings.array.every((monoring) => monoring.isEnded);
