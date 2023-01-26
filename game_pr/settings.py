9b8a97a5395ea37627f6f892aa067bf3fb56561d
Lizazal <66370193+Lizazal@users.noreply.github.com>
2023-01-26 12:48:43 +0300

    Update settings.py

--- game_pr/settings.py
+++ game_pr/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-nuw_+iut4*-a5iq)c%14j6$xs(vn+nl(94_5zwscy#2+3#ex4+"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
